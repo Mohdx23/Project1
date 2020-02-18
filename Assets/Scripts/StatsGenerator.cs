138c89aa84e4eed8ca588fa561e67584a3b99475
Mohammed Ahmed <m.hussein@saedubai.com>
2020-02-18 19:01:36 +0400

    GAD170 project added
    Week 4

---
+++ Assets/Scripts/StatsGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+///  A <see langword="static"/> class with methods (functions) for initialising or randomising the stats class.
+///  
+/// TODO:
+///     Initialise a stats instance with generated starting stats
+///     Handle the assignment of extra points or xp into an existing stats of a character
+///         - this is expected to be used by NPCs leveling up to match the player.
+/// </summary>
+public static class StatsGenerator
+{
+    public static void InitialStats(Stats stats)
+    {
+        int totalPoints = 10;
+        stats.luck = Random.Range(0, totalPoints);
+        totalPoints -= stats.luck;
+
+        stats.rhythm = Random.Range(0, totalPoints);
+        totalPoints -= stats.rhythm;
+
+        stats.style = totalPoints;
+
+        stats.level = 1;
+        stats.xp = 0;
+    }
+
+    public static void AssignUnusedPoints(Stats stats, int points)
+    {
+    }
+}
---
+++ Assets/Scripts/XPHandler.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class is responsible for converting a battle result into xp to be awarded to the player.
+/// 
+/// TODO:
+///     Respond to battle outcome with xp calculation based on;
+///         player win 
+///         how strong the win was
+///         stats/levels of the dancers involved
+///     Award the calculated XP to the player stats
+///     Raise the player level up event if needed
+/// </summary>
+public class XPHandler : MonoBehaviour
+{
+    private void OnEnable()
+    {
+        GameEvents.OnBattleConclude += GainXP;
+
+    }
+
+    private void OnDisable()
+    {
+    }
+
+    public void GainXP(BattleResultEventData data)
+    {
+        int initialxp = 0;
+
+        if (data.outcome > 0)
+            data.player.xp += 25;
+
+        int xprequired = data.player.level * 250 + initialxp;
+
+        if (data.player.xp >= xprequired)
+        {
+         data.player.xp = 0;
+        data.player.level += 1;
+        GameEvents.PlayerLevelUp(data.player.level);
+                
+        }
+                
+                //GameEvents.PlayerXPGAIN(100); << TO MAKE IT APPEAR ON THE SCREEN
+                //GameEvents.PlayerLevelUp(1);
+                
+            
+                
+
+    }
+
+}
